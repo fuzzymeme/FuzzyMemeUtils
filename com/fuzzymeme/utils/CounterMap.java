f0a463c7cd70e45a2c36dce1f330309a7c3b178c
fuzzymeme <fuzzymeme@gmail.com>
2017-05-04 21:42:55 +0100

    Utils for stuff like pretty prints

--- com/fuzzymeme/utils/CounterMap.java
+++ com/fuzzymeme/utils/CounterMap.java
@@ -1,30 +1,56 @@
 package com.fuzzymeme.utils;
 
-public class CounterMap<T> {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class CounterMap<K> {
 	
-	private int count = 0;
-	private final T payload;
+	private Map<K, Integer> map = new HashMap<K, Integer>();
 	
-	public CounterMap(T payload){
-		this.payload = payload;
+	public void remove(K key){
+		if(!map.containsKey(key)){
+			return;
+		}
+		map.remove(key);
 	}
 	
-	public void incr(){
-		count++;
+	public void inc(K key){
+		
+		if(!map.containsKey(key)){
+			map.put(key, new Integer(0));
+		}
+		
+		map.put(key, map.get(key) + 1);
 	}
 
-	public int getCount(){
-		return count;
-	}
-	
-	public T getPayload(){
-		return payload;
+	public void set(K key, int count){		
+		map.put(key, count);
 	}
 
+	public boolean containsKey(K key){
+		return map.containsKey(key);
+	}
+	
+	public int get(K key){
+		if(map.containsKey(key)){
+			return map.get(key);
+		} else {
+			return 0;
+		}
+	}
+	
+	public Set<K> keySet(){
+		return map.keySet();
+	}
+		
+	public int size(){
+		return map.size();
+	}
+	
 	@Override
 	public String toString() {
-		return "[count=" + count + ":" + payload + "]";
+		return map.toString();
 	}
-	
-	
 }
+
