11fc43a898f17f849e65dd094f48da5b193faa06
fuzzymeme <fuzzymeme@gmail.com>
2017-06-29 13:00:20 +0100

    Tweak to test - cleaning code

--- com/fuzzymeme/utils/io/StreamingTableReaderTests.java
+++ com/fuzzymeme/utils/io/StreamingTableReaderTests.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 
 public class StreamingTableReaderTests {
 	
+	private final static String TEST_FILES_DIR = "src/test_files/";
+	
 	private final static TableRow exampleRowZero = new TableRow(new String[]{"a", "b", "c", "d", "e"});
 	private final static TableRow exampleRowOne = new TableRow(new String[]{"f", "g", "h", "i", "j"});
 	private final static String[] exampleHeaders = new String[]{"one", "two", "three", "four", "five"};
@@ -19,7 +21,7 @@ public class StreamingTableReaderTests {
 		
 		try {
 			StreamingTableReader loader = new StreamingTableReader(",");
-			Table table = loader.loadTable("test_table.txt");
+			Table table = loader.loadTable(TEST_FILES_DIR + "test_table.txt");
 			assertEquals(exampleRowZero, table.getRow(0));
 			assertEquals(exampleRowOne, table.getRow(1));
 		} catch (IOException e) {
@@ -33,7 +35,7 @@ public class StreamingTableReaderTests {
 		
 		try {
 			StreamingTableReader loader = new StreamingTableReader(",");
-			Table table = loader.loadTable("test_table.txt");
+			Table table = loader.loadTable(TEST_FILES_DIR + "test_table.txt");
 			assertEquals(exampleHeaders.length, table.getColumnCount());
 			for(int i = 0; i < table.getColumnCount(); i++) {
 				assertEquals(exampleHeaders[i], table.getHeader(i));
@@ -49,7 +51,7 @@ public class StreamingTableReaderTests {
 		
 		try {
 			StreamingTableReader loader = new StreamingTableReader(" ");
-			Table table = loader.loadTable("test_table_space_separated.txt");
+			Table table = loader.loadTable(TEST_FILES_DIR + "test_table_space_separated.txt");
 			assertEquals(exampleRowZero, table.getRow(0));
 			assertEquals(exampleRowOne, table.getRow(1));
 		} catch (IOException e) {
@@ -63,7 +65,7 @@ public class StreamingTableReaderTests {
 		
 		try {
 			StreamingTableReader loader = new StreamingTableReader(" ");
-			Table table = loader.loadTable("test_table_space_separated.txt");
+			Table table = loader.loadTable(TEST_FILES_DIR + "test_table_space_separated.txt");
 			assertEquals(exampleHeaders.length, table.getColumnCount());
 			for(int i = 0; i < table.getColumnCount(); i++) {
 				assertEquals(exampleHeaders[i], table.getHeader(i));
@@ -79,7 +81,7 @@ public class StreamingTableReaderTests {
 		
 		try {
 			StreamingTableReader loader = new StreamingTableReader("\t");
-			Table table = loader.loadTable("test_table_tab_separated.txt");
+			Table table = loader.loadTable(TEST_FILES_DIR + "test_table_tab_separated.txt");
 			assertEquals(exampleRowZero, table.getRow(0));
 			assertEquals(exampleRowOne, table.getRow(1));
 		} catch (IOException e) {
@@ -93,7 +95,7 @@ public class StreamingTableReaderTests {
 		
 		try {
 			StreamingTableReader loader = new StreamingTableReader("\t");
-			Table table = loader.loadTable("test_table_tab_separated.txt");
+			Table table = loader.loadTable(TEST_FILES_DIR + "test_table_tab_separated.txt");
 			assertEquals(exampleHeaders.length, table.getColumnCount());
 			for(int i = 0; i < table.getColumnCount(); i++) {
 				assertEquals(exampleHeaders[i], table.getHeader(i));
@@ -109,7 +111,7 @@ public class StreamingTableReaderTests {
 		
 		try {
 			StreamingTableReader loader = new StreamingTableReader("***");
-			Table table = loader.loadTable("test_table_token_separated.txt");
+			Table table = loader.loadTable(TEST_FILES_DIR + "test_table_token_separated.txt");
 			assertEquals(exampleRowZero, table.getRow(0));
 			assertEquals(exampleRowOne, table.getRow(1));
 		} catch (IOException e) {
@@ -123,7 +125,7 @@ public class StreamingTableReaderTests {
 		
 		try {
 			StreamingTableReader loader = new StreamingTableReader("***");
-			Table table = loader.loadTable("test_table_token_separated.txt");
+			Table table = loader.loadTable(TEST_FILES_DIR + "test_table_token_separated.txt");
 			assertEquals(exampleHeaders.length, table.getColumnCount());
 			for(int i = 0; i < table.getColumnCount(); i++) {
 				assertEquals(exampleHeaders[i], table.getHeader(i));
